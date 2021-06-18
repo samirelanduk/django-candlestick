c728cd7dacb821bfe470690b9f93e074b329186a
Sam Ireland <sam.ireland.uk@gmail.com>
2021-06-18 22:22:13 +0100

    Basic price model

--- candlestick/models.py
+++ candlestick/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
 class Instrument(models.Model):
     """A tradeable entity."""
@@ -10,3 +11,20 @@ class Instrument(models.Model):
     exchange = models.CharField(max_length=20, blank=True, null=True)
     currency = models.CharField(max_length=20)
     category = models.CharField(max_length=100)
+
+
+
+class Price(models.Model):
+    """The price of an instrument over some period of time."""
+
+    class Meta:
+        ordering = ["timestamp"]
+
+    timestamp = models.IntegerField()
+    resolution = models.CharField(validators=[RegexValidator("^\d{0,2}[smHDWMY]$")], max_length=3)
+    open = models.DecimalField(max_digits=9, decimal_places=5)
+    low = models.DecimalField(max_digits=9, decimal_places=5)
+    high = models.DecimalField(max_digits=9, decimal_places=5)
+    close = models.DecimalField(max_digits=9, decimal_places=5)
+    volume = models.IntegerField()
+    instrument = models.ForeignKey(Instrument, on_delete=models.CASCADE, related_name="prices")
